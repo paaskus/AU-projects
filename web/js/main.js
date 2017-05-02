ce8baee5f7055f1c727a4afbfac7add6bc2fcf9f
Bardur R. Guttesen <bardurrunason@gmail.com>
2017-05-02 09:51:25 +0200

    Capture filters in frontend and output JSON

--- web/js/main.js
+++ web/js/main.js
@@ -19,8 +19,29 @@ $(document).ready(function() {
 
     $('#result-page').hide();
     $('#result-button').click(function() {
+        var configuration = {};
+        var filters = getFilters();
+        configuration.filters = filters;
+        console.log(JSON.stringify(configuration));
         drawCharts();
         $('#result-page').show();
         $('html, body').animate({ scrollTop: $('#result-page').offset().top }, 1500);
     });
 });
+
+function getFilters() {
+    var filters = [];
+    $('.filter-section').each(function() {
+        var attributeName = $(this).attr('data-attribute-name');
+        var $selectedFilters = $(this).find('.selected');
+        var selectedFilterNames = [];
+        $selectedFilters.each(function() {
+            selectedFilterNames.push($(this).attr('data-value-name'));
+        });
+
+        if (selectedFilterNames.length > 0) {
+            filters.push({filterName: attributeName, selected: selectedFilterNames});
+        }
+    });
+    return filters;
+}
